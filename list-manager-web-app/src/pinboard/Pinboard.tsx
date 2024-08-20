b9ce139877e4e5206223f76d4756c650e88c5c43
Rowan5654 <Rowan5654@gmail.com>
2024-08-20 12:03:09 +1200

    added and implemented the 'Pinboard' component

---
+++ list-manager-web-app/src/pinboard/Pinboard.tsx
@@ -0,0 +1,12 @@
+function Pinboard() {
+    return (
+        <div className="root-pinboard">
+            <div className="new-list-dialogue-container">
+                <p>Create new list</p>
+                <p>+</p>
+            </div>
+        </div>
+    );
+}
+
+export default Pinboard;
